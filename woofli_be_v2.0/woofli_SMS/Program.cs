6b79b483b129845af22f2dec4008c412fce874be
MATT BRUTON <the.bruton@gmail.com>
2017-01-08 23:30:00 -0600

    added console app to push emails

---
+++ woofli_be_v2.0/woofli_SMS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Typesafe.Mailgun;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using woofli_be_v2._0.DAL;
+using woofli_be_v2._0.Models;
+
+namespace woofli_SMS
+{
+    class Program
+    {
+        public static bool runProgram = true;
+
+
+
+        static void Main(string[] args)
+        {
+            while (runProgram)
+            {
+                AuthRepository repo = new AuthRepository();
+                List<CustomUser> users = repo.GetAllUsers();
+
+                foreach (var user in users)
+                {
+                    foreach (var pet in user.Pets)
+                    {
+                        if (pet.Medications.Any(m => m.DosageTime > DateTime.Now && m.DosageTime < DateTime.Now.AddHours(1)))
+                        {
+                            List<Medicine> meds_with_upcoming_message = pet.Medications.Where((m => m.DosageTime > DateTime.Now && m.DosageTime < DateTime.Now.AddMinutes(10))).ToList();
+
+                            foreach (var med in meds_with_upcoming_message)
+                            {
+                                var client = new MailgunClient("sandboxb97fd526243f498e99be4b242a0c8b43.mailgun.org", SecretKey.ApiKey, 3);
+
+                                client.SendMail(new System.Net.Mail.MailMessage("reminder@woof.li", user.Email)
+                                {
+                                    Subject = "Medication Reminder for " + pet.Name,
+                                    Body = pet.Name + " needs their dose of " + med.Name + " at " + med.DosageTime.ToShortTimeString() + "."
+                                });
+
+                                Console.WriteLine(pet.Name + " needs his or her next dose of " + med.Name + " at " + med.DosageTime.ToShortTimeString() + " -- From woof.li");
+                            }
+                        }
+                    }
+                }
+
+                Thread.Sleep(10 * 60 * 1000);
+            }
+            
+        }
+    }
+}
---
+++ woofli_be_v2.0/woofli_be_v2.0/Migrations/201701082036331_RevertPet.cs
@@ -0,0 +1,16 @@
+namespace woofli_be_v2._0.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RevertPet : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
